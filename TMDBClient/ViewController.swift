7bc2b97ae01c9ea35142b267c775fc923c18a712
Prayash Thapa <prayasht@gmail.com>
2020-02-23 14:43:48 -0700

    Initial Commit

---
+++ TMDBClient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TMDBClient
+//
+//  Created by Prayash Thapa on 2/23/20.
+//  Copyright © 2020 Viget. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
